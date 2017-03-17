02bd9e9d25e872c3e4a25047b108ba14ffda7a29
陈子谦 <chenziqian@charrr.local>
2017-03-17 17:45:13 +0800

    Initial Commit

---
+++ Test3DLabs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test3DLabs
+//
+//  Created by 陈子谦 on 2017/3/17.
+//  Copyright © 2017年 Charrr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
